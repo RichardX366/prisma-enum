bc108d8e4e1a43244f767779904525ef9cf1fb15
Richard X <richardx366@gmail.com>
2022-09-05 12:59:26 -0400

    works?

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Instructions
+
+Put ` && prisma-enum` in your build step after `prisma generate` but before your actual build.
+Put ` && prisma-enum` in your schema update step after the update.
--- run.js
+++ run.js
@@ -1,14 +1,28 @@
 #!/usr/bin/env node
 
 const { createSpinner } = require('nanospinner');
-const { readFileSync } = require('fs');
+const { readFileSync, writeFIleSync, writeFileSync } = require('fs');
 
 const spinner = createSpinner('Creating prisma declarations...').start();
 const prismaText = readFileSync(
   './node_modules/.prisma/client/index.d.ts',
   'utf8',
 );
-console.log(prismaText);
-spinner.stop();
+writeFileSync(
+  './node_modules/prisma-enum/index.d.ts',
+  prismaText
+    .split(
+      '// https://github.com/microsoft/TypeScript/issues/3192#issuecomment-261720275',
+    )[1]
+    .split('/**')[0],
+);
+const enumJS = prismaText
+  .split('exports.Prisma.ModelName')[0]
+  .split('exports.Prisma');
+writeFileSync(
+  './node_modules/prisma-enum/index.js',
+  enumJS[enumJS.length - 1].split('});').slice(1).join('});'),
+);
+spinner.success();
 
 process.exit(0);
