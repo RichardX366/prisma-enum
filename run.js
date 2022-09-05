b3086f4a7dc9edaf388fafd9a5216ef735b6f360
Richard X <richardx366@gmail.com>
2022-09-05 13:22:58 -0400

    works?

--- run.js
+++ run.js
@@ -4,22 +4,22 @@ const { createSpinner } = require('nanospinner');
 const { readFileSync, writeFIleSync, writeFileSync } = require('fs');
 
 const spinner = createSpinner('Creating prisma declarations...').start();
-const prismaText = readFileSync(
+const prismaTS = readFileSync(
   './node_modules/.prisma/client/index.d.ts',
   'utf8',
 );
 writeFileSync(
   './node_modules/prisma-enum/index.d.ts',
-  prismaText
+  prismaTS
     .split(
       '// https://github.com/microsoft/TypeScript/issues/3192#issuecomment-261720275',
     )[1]
     .split('/**')[0],
 );
-const enumJS = prismaText
+const prismaJS = readFileSync('./node_modules/.prisma/client/index.js', 'utf8');
+const enumJS = prismaJS
   .split('exports.Prisma.ModelName')[0]
   .split('exports.Prisma');
-console.log(enumJS);
 writeFileSync(
   './node_modules/prisma-enum/index.js',
   `function makeEnum(x) { return x; }
